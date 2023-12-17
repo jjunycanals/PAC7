ec76d2ba3543121582cb530ea5eb1808016887fa
jordi juny <jjunyc@uoc.edu>
2023-12-17 23:23:07 +0100

    fi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { ArticleItemComponent } from './article-item/article-item.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ArticleNewTemplateComponent } from './article-new-template/article-new-template.component';
 import { ArticleNewReactiveComponent } from './article-new-reactive/article-new-reactive.component';
+import { ArticleListComponent } from './article-list/article-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleItemComponent,
+    ArticleListComponent,
     NavbarComponent,
     ArticleNewTemplateComponent,
     ArticleNewReactiveComponent
